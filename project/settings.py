dca35b8995d1e3137c49c282c59696deaa3000bc
Darren Robinson <darrenandamanda.robinson@gmail.com>
2026-04-28 11:45:54 +1000

    Initial commit

--- project/settings.py
+++ project/settings.py
@@ -85,7 +85,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 # WhiteNoise serves the React build from root-level URLs (e.g. /assets/..., /index.html)
 WHITENOISE_ROOT = BASE_DIR / 'staticfiles' / 'frontend'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
